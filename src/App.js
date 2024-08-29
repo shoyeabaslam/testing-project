857e02a9474ef2c5b109a1febba73d2fe81c6acd
shoyeabaslam <shoyeab07@gmail.com>
2024-08-29 10:58:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Aila - React Project</h1>
+      <label htmlFor="title">Aila Sample</label>
+      <br/>
+      <label htmlFor="data">Aila Scanned Data</label>
+    </div>
+  );
+};
+ 
+export default App;
